440256655ad05d64e3c3096ff71f193df53f435d
kkudyba <kamila.kudyba14@stjohns.edu>
2016-11-17 18:31:45 -0500

    Work in Progress so far

--- src/main/java/com/jkmalan/adoptafriend/database/Database.java
+++ src/main/java/com/jkmalan/adoptafriend/database/Database.java
@@ -3,48 +3,88 @@ package com.jkmalan.adoptafriend.database;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Database {
 
-    public Database() {
+	private Connection connect = null;
 
-    }
+	public Database() {
 
-    public void connect() {
+	}
 
-    }
+	// tests the connection
 
-    public void disconnect() {
+	public boolean connect() {
 
-    }
+		try {
+			getConnection();
+			return true;
+		} catch (SQLException ex) {
+			return false;
+		}
+	}
 
-    public Connection getConnection() {
-        return null;
-    }
+	public void disconnect() {
 
-    public void setConnection(Connection conn) {
+	}
 
-    }
+	// gets the connection
 
-    public void executeStatement(String query) {
+	public Connection getConnection() throws SQLException {
+		if (connect == null || connect.isClosed()) {
+			getConnection();
+		}
 
-    }
+		return null;
+	}
 
-    public Statement getStatement(String query) {
-        return null;
-    }
+	// reactivates the connection
 
-    public PreparedStatement getPreparedStatement(String query) {
-        return null;
-    }
+	public void setConnection(Connection connect) {
 
-    public void closeStatement(Statement statement) {
+		this.connect = connect;
+	}
 
-    }
+	// creates a new Statement and executes it
 
-    public void closeResultSet(ResultSet result) {
+	public void executeStatement(String query) throws SQLException {
 
-    }
+		Statement statement = getStatement(query);
+		statement.execute(query);
+		closeStatement(statement);
+
+	}
+
+	public Statement getStatement(String query) {
+		return null;
+	}
+
+	// creates a new PreparedStatement
+
+	public PreparedStatement getPreparedStatement(String query) throws SQLException {
+		return getConnection().prepareStatement(query);
+
+		// return null;
+	}
+
+	// closes a Statement
+
+	public void closeStatement(Statement statement) throws SQLException {
+		if (statement != null) {
+			statement.close();
+		}
+
+	}
+
+	// closes a ResultSet
+
+	public void closeResultSet(ResultSet result) throws SQLException {
+		if (result != null) {
+			result.close();
+		}
+
+	}
 
 }
--- src/main/java/com/jkmalan/adoptafriend/database/DatabaseManager.java
+++ src/main/java/com/jkmalan/adoptafriend/database/DatabaseManager.java
@@ -1,21 +1,44 @@
 package com.jkmalan.adoptafriend.database;
 
+import java.sql.SQLException;
+
 public class DatabaseManager {
 
-    public DatabaseManager() {
+	private final Database database;
 
-    }
+	public DatabaseManager() {
+		database = new Database();
+	}
 
-    public void checkTables() {
+	// checks the tables
 
-    }
+	public void checkTables() {
 
-    public void createTables() {
+	}
 
-    }
+	// creates tables for Profile and Listings
 
-    public void updateTables() {
+	public void createTables() throws SQLException {
 
-    }
+		String PROFILE_TABLE = "CREATE TABLE PROFILE ( " + " UID    CHAR   NOT NULL," + " UFName  CHAR(25)  NOT NULL,"
+				+ " ULName   CHAR(25) NOT NULL," + " UEmail CHAR(60)  NOT NULL," + " UStreet CHAR(45) NOT NULL,"
+				+ " UState  CHAR(2) NOT NULL," + " UZip    CHAR(10) NOT NULL," + " UPhone INT(10),"
+				+ " UDescription CHAR(255)," + " PRIMARY KEY(UID));";
+		String LISTING_TABLE = " CREATE TABLE LISTING ( " + " LID  CHAR(15)  NOT NULL, "
+				+ " LTitle CHAR(40)  NOT NULL, " + " LAge   INT(2)    NOT NULL, " + " LType  CHAR(40)   NOT NULL, "
+				+ " LSex   CHAR(1)     NOT NULL, " + " LAttributes CHAR(255) NOT NULL, " + " LDescription  CHAR(255), "
+				+ " LRegion     CHAR(60)   NOT NULL, " + " PRIMARY KEY(LID), "
+				+ " FOREIGN KEY(LID) REFERENCES PROFILE(UID)); ";
+
+		database.executeStatement(PROFILE_TABLE);
+		database.executeStatement(LISTING_TABLE);
+
+	}
+
+	// updates the tables
+
+	public void updateTables() {
+
+	}
 
 }
