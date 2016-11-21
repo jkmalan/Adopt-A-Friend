3d9efad9d6dd3a7ce22b10bf50b43e4681d49943
jkmalan <jkmalan@admincmd.com>
2016-11-21 13:33:07 -0500

    [ADD] Add ListingManager, Listing, UserManager, SearchManager, and AppEngine

---
+++ src/main/java/com/jkmalan/adoptafriend/user/User.java
@@ -0,0 +1,28 @@
+package com.jkmalan.adoptafriend.user;
+
+public class User {
+
+    private String username;
+    protected Integer userId;
+    private String password;
+
+    public User(String username, String password, Integer userId) {
+
+        this.username = username;
+        this.password = password;
+        this.userId = userId;
+    }
+
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+
+    public String getUserName() {
+        return username;
+    }
+
+
+}
+
