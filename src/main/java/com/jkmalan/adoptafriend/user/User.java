d637c7d22fb1393e945c7bc2f7801d21988e40d2
CelesteGallardo <gallardo.cg@gmail.com>
2016-12-06 13:47:43 -0500

    Updated User class

--- src/main/java/com/jkmalan/adoptafriend/user/User.java
+++ src/main/java/com/jkmalan/adoptafriend/user/User.java
@@ -1,27 +1,127 @@
 package com.jkmalan.adoptafriend.user;
 
+import java.util.UUID;
+
 public class User {
 
-    private String username;
+	private final UUID uuid;
+	private String username;
     protected Integer userId;
     private String password;
+    private String fName;
+    private String lName;
+    private String emailAddress;
+    private String phoneNumber;
+    private String streetAddress;
+    private String state;
+    private String zipCode;
+    private String bio;
+    
+  /*  private List<File> ;
+    private List<String>;*/
 
-    public User(String username, String password, Integer userId) {
+    public User(UUID uuid)
+    {
 
-        this.username = username;
-        this.password = password;
-        this.userId = userId;
+        this.uuid=uuid;
+    }
+    
+    public UUID getUUID()
+    {
+	return uuid;
     }
 
-
-    public Integer getUserId() {
+    public Integer getUserId() 
+    {
         return userId;
     }
 
-
-    public String getUserName() {
+    public String getUserName() 
+    {
         return username;
+    }   
+    
+    public void setFistName(String fName)
+    {
+    	this.fName=fName;
     }
+    
+    public String getFirstName()
+    {
+    	return fName;
+    }
+    
+    public void setLastName(String lName)
+    {
+    	this.lName=lName;
+    }
+    
+    public String getLastName()
+    {
+    	return lName;
+    }
+    
+    public void setEmail(String emailAddress)
+    {
+    	this.emailAddress=emailAddress;
+    }
+    
+    public String getEmail()
+    {
+    	return emailAddress;
+    }
+    
+    public void setPhoneNumber(String phoneNumber)
+    {
+    	this.phoneNumber=phoneNumber;
+    }
+    
+    public String getPhoneNum()
+    {
+    	return phoneNumber;
+    }
+    
+    public void setStreetAddress(String streetAddress)
+    {
+    	this.streetAddress=streetAddress;
+    }
+    
+    public String getAddress()
+    {
+    	return streetAddress;
+    }
+    
+    public void setState(String state)
+    {
+    	this.state=state;	
+    }
+    
+    public String getState()
+    {
+    	return state;
+    }
+    
+    public void setZipCode(String zipCode)
+    {
+    	this.zipCode=zipCode;
+    }
+    
+    public String getZip()
+    {
+    	return zipCode;
+    }
+    
+    public void setBio(String bio)
+    {
+    	this.bio=bio;
+    }
+    
+    public String getBio()
+    {
+		return bio;
+    }
+    
+    
 
 
 }
