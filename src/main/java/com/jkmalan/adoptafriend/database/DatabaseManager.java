58ac0c6f886b5ef1ee73e5ffb3b41c2d14789545
jkmalan <jkmalan@admincmd.com>
2016-12-07 12:29:43 -0500

    [MOD] Fill in remaining SQL methods and begin hashing work

--- src/main/java/com/jkmalan/adoptafriend/database/DatabaseManager.java
+++ src/main/java/com/jkmalan/adoptafriend/database/DatabaseManager.java
@@ -1,8 +1,8 @@
 package com.jkmalan.adoptafriend.database;
 
 import com.jkmalan.adoptafriend.listing.Listing;
+import com.jkmalan.adoptafriend.user.Account;
 import com.jkmalan.adoptafriend.user.User;
-import com.mysql.jdbc.ResultSetImpl;
 
 import java.io.File;
 import java.sql.PreparedStatement;
@@ -21,17 +21,19 @@ public class DatabaseManager {
 		database = new Database(file);
         try {
             database.connect();
+            database.getStatement().execute("PRAGMA foreign_keys = ON;");
         } catch (SQLException e) {
             // TODO Handle errors
         }
     }
 
 	public void createTables() throws SQLException {
-        String ACCOUNT_TABLE = "CREATE TABLE IF NOT EXISTS `account` ("
+        String ACCOUNT_TABLE = "CREATE TABLE IF NOT EXISTS `account` ( "
                 + "`uid` INTEGER NOT NULL"
+                + "`salt` BYTE NOT NULL"
                 + "`hash` CHAR(40) NOT NULL"
                 + "PRIMARY KEY (`uid`)"
-                + "FOREIGN KEY (`uid`) REFERENCES USER (`uid`)"
+                + "FOREIGN KEY (`uid`) REFERENCES USER (`uid`) ON DELETE CASCADE"
                 + ");";
         String USER_TABLE = "CREATE TABLE IF NOT EXISTS `user` ( "
                 + "`uid` INTEGER NOT NULL AUTO_INCREMENT"
@@ -45,6 +47,7 @@ public class DatabaseManager {
                 + "`desc` CHAR(256)"
                 + "`photo` CHAR(128)"
                 + "PRIMARY KEY (`uid`)"
+                + "UNIQUE (`name`)"
                 + ");";
         String LISTING_TABLE = "CREATE TABLE IF NOT EXISTS `listing` ( "
                 + "`lid` INTEGER NOT NULL AUTO_INCREMENT"
@@ -57,41 +60,235 @@ public class DatabaseManager {
                 + "`desc` CHAR(256)"
                 + "`photo` CHAR(128)"
                 + "PRIMARY KEY (`lid`)"
-                + "FOREIGN KEY (`owner`) REFERENCES `user` (`uid`)"
+                + "FOREIGN KEY (`owner`) REFERENCES `user` (`uid`) ON DELETE CASCADE"
+                + "UNIQUE (`title`)"
                 + ");";
         database.getStatement().execute(USER_TABLE);
 		database.getStatement().execute(ACCOUNT_TABLE);
 		database.getStatement().execute(LISTING_TABLE);
 	}
 
-    public void insertUser() {
-        String USER_INSERT = "";
-    }
-
-	public void insertListing() {
-        String LISTING_INSERT = "";
-    }
-
-    public void updateUser() {
-        String USER_UPDATE = "";
-    }
-
-    public void updateListing() {
-        String LISTING_UPDATE = "";
-    }
-
-    public void deleteUser() {
-        String USER_DELETE = "";
-    }
-
-    public void deleteListing() {
-        String LISTING_DELETE = "";
-    }
-
-    public Optional<User> selectUser(Integer uid) {
-        String USER_QUERY = "SELECT * FROM `user` WHERE `uid` = ?;";
+    public void insertUser(String name, String email, String phone, String street, String city, String state, String zip, String desc, String photo) {
+        String USER_INSERT = "INSERT INTO `user` (`name`, `email`, `phone`, `street`, `city`, `state`, `zip`, `desc`, `photo`) "
+                + "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);";
+        PreparedStatement ps = null;
         try {
-            PreparedStatement ps = database.getPreparedStatement(USER_QUERY);
+            ps = database.getPreparedStatement(USER_INSERT);
+            ps.setString(1, name);
+            ps.setString(2, email);
+            ps.setString(3, phone);
+            ps.setString(4, street);
+            ps.setString(5, city);
+            ps.setString(6, state);
+            ps.setString(7, zip);
+            ps.setString(8, desc);
+            ps.setString(9, photo);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            // TODO Handles errors
+        } finally {
+            try {
+                if (ps != null) {
+                    ps.close();
+                }
+            } catch (SQLException e) {
+                // TODO Handles errors
+            }
+        }
+    }
+
+    public void insertAccount(int uid, byte[] salt, String hash) {
+        String ACCOUNT_INSERT = "INSERT INTO `account` (`uid`, `salt`, `hash`) VALUES (?, ?, ?);";
+        PreparedStatement ps = null;
+        try {
+            ps = database.getPreparedStatement(ACCOUNT_INSERT);
+            ps.setInt(1, uid);
+            ps.setBytes(2, salt);
+            ps.setString(3, hash);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            // TODO Handles errors
+        } finally {
+            try {
+                if (ps != null) {
+                    ps.close();
+                }
+            } catch (SQLException e) {
+                // TODO Handles errors
+            }
+        }
+    }
+
+	public void insertListing(int owner, String title, String zip, String type, String sex, int age, String desc, String photo) {
+        String LISTING_INSERT = "INSERT INTO `listing` (`owner`, `title`, `zip`, `type`, `sex`, `age`, `desc`, `photo`) "
+                + "VALUES (?, ?, ?, ?, ?, ?, ?, ?);";
+        PreparedStatement ps = null;
+        try {
+            ps = database.getPreparedStatement(LISTING_INSERT);
+            ps.setInt(1, owner);
+            ps.setString(2, title);
+            ps.setString(3, zip);
+            ps.setString(4, type);
+            ps.setString(5, sex);
+            ps.setInt(6, age);
+            ps.setString(7, desc);
+            ps.setString(8, photo);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            // TODO Handle errors
+        } finally {
+            try {
+                if (ps != null) {
+                    ps.close();
+                }
+            } catch (SQLException e) {
+                // TODO Handles errors
+            }
+        }
+    }
+
+    public void updateUser(int uid, String name, String email, String phone, String street, String city, String state, String zip, String desc, String photo) {
+        String USER_UPDATE = "UPDATE `user` SET `name` = ?, `email` = ?, `phone` = ?, `street` = ?, "
+                + "`city` = ?, `state` = ?, `zip` = ?, `desc` = ?, `photo` = ? WHERE `uid` = ?;";
+        PreparedStatement ps = null;
+        try {
+            ps = database.getPreparedStatement(USER_UPDATE);
+            ps.setString(1, name);
+            ps.setString(2, email);
+            ps.setString(3, phone);
+            ps.setString(4, street);
+            ps.setString(5, city);
+            ps.setString(6, state);
+            ps.setString(7, zip);
+            ps.setString(8, desc);
+            ps.setString(9, photo);
+            ps.setInt(10, uid);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            // TODO Handle errors
+        } finally {
+            try {
+                if (ps != null) {
+                    ps.close();
+                }
+            } catch (SQLException e) {
+                // TODO Handles errors
+            }
+        }
+    }
+
+    public void updateListing(int lid, String title, String zip, String type, String sex, int age, String desc, String photo) {
+        String LISTING_UPDATE = "UPDATE `listing` SET `title` = ?, `zip` = ?, `type` = ?, "
+                + "`sex` = ?, `age` = ?, `desc` = ?, `photo` = ? WHERE `lid` = ?;";
+        PreparedStatement ps = null;
+        try {
+            ps = database.getPreparedStatement(LISTING_UPDATE);
+            ps.setString(1, title);
+            ps.setString(2, zip);
+            ps.setString(3, type);
+            ps.setString(4, sex);
+            ps.setInt(5, age);
+            ps.setString(6, desc);
+            ps.setString(7, photo);
+            ps.setInt(8, lid);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            // TODO Handle errors
+        } finally {
+            try {
+                if (ps != null) {
+                    ps.close();
+                }
+            } catch (SQLException e) {
+                // TODO Handles errors
+            }
+        }
+    }
+
+    public void deleteUser(int uid) {
+        String USER_DELETE = "DELETE FROM `user` WHERE `uid` = ?;";
+        PreparedStatement ps = null;
+        try {
+            ps = database.getPreparedStatement(USER_DELETE);
+            ps.setInt(1, uid);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            // TODO Handle errors
+        } finally {
+            try {
+                if (ps != null) {
+                    ps.close();
+                }
+            } catch (SQLException e) {
+                // TODO Handles errors
+            }
+        }
+    }
+
+    public void deleteListing(int lid) {
+        String LISTING_DELETE = "DELETE FROM `listing` WHERE `uid` = ?;";
+        PreparedStatement ps = null;
+        try {
+            ps = database.getPreparedStatement(LISTING_DELETE);
+            ps.setInt(1, lid);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            // TODO Handle errors
+        } finally {
+            try {
+                if (ps != null) {
+                    ps.close();
+                }
+            } catch (SQLException e) {
+                // TODO Handles errors
+            }
+        }
+    }
+
+    public Optional<User> selectUser(String name) {
+        String USER_QUERY = "SELECT * FROM `user` WHERE `name` = ?;";
+        PreparedStatement ps = null;
+        try {
+            ps = database.getPreparedStatement(USER_QUERY);
+            ps.setString(1, name);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                User user = new User(rs.getInt("uid"));
+
+                user.setName(rs.getString("name"));
+                user.setEmail(rs.getString("email"));
+                user.setPhone(rs.getString("phone"));
+                user.setStreet(rs.getString("street"));
+                user.setCity(rs.getString("city"));
+                user.setState(rs.getString("state"));
+                user.setZip(rs.getString("zip"));
+                user.setDesc(rs.getString("desc"));
+
+                String path = rs.getString("photo");
+                File file = new File(path);
+                user.setPhoto(file);
+
+                return Optional.of(user);
+            }
+        } catch (SQLException e) {
+            // TODO Handle errors
+        } finally {
+            try {
+                if (ps != null) {
+                    ps.close();
+                }
+            } catch (SQLException e) {
+                // TODO Handles errors
+            }
+        }
+        return Optional.empty();
+    }
+
+    public Optional<User> selectUser(int uid) {
+        String USER_QUERY = "SELECT * FROM `user` WHERE `uid` = ?;";
+        PreparedStatement ps = null;
+        try {
+            ps = database.getPreparedStatement(USER_QUERY);
             ps.setInt(1, uid);
             ResultSet rs = ps.executeQuery();
             if (rs.next()) {
@@ -109,17 +306,90 @@ public class DatabaseManager {
                 String path = rs.getString("photo");
                 File file = new File(path);
                 user.setPhoto(file);
+
+                return Optional.of(user);
             }
         } catch (SQLException e) {
             // TODO Handle errors
+        } finally {
+            try {
+                if (ps != null) {
+                    ps.close();
+                }
+            } catch (SQLException e) {
+                // TODO Handles errors
+            }
         }
         return Optional.empty();
     }
 
-    public Optional<Listing> selectListing(Integer lid) {
-        String LISTING_QUERY = "SELECT * FROM `listing` WHERE `lid` = ?;";
+    public Optional<Account> selectAccount(int uid) {
+        String ACCOUNT_QUERY = "SELECT * FROM `account` WHERE `uid` = ?;";
+        PreparedStatement ps = null;
         try {
-            PreparedStatement ps = database.getPreparedStatement(LISTING_QUERY);
+            ps = database.getPreparedStatement(ACCOUNT_QUERY);
+            ps.setInt(1, uid);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                Account account = new Account(rs.getInt("uid"), rs.getBytes("salt"), rs.getString("hash"));
+                return Optional.of(account);
+            }
+        } catch (SQLException e) {
+            // TODO Handle errors
+        } finally {
+            try {
+                if (ps != null) {
+                    ps.close();
+                }
+            } catch (SQLException e) {
+                // TODO Handles errors
+            }
+        }
+        return Optional.empty();
+    }
+
+    public Optional<Listing> selectListing(String title) {
+        String LISTING_QUERY = "SELECT * FROM `listing` WHERE `title` = ?;";
+        PreparedStatement ps = null;
+        try {
+            ps = database.getPreparedStatement(LISTING_QUERY);
+            ps.setString(1, title);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                Listing listing = new Listing(rs.getInt("lid"), rs.getInt("owner"));
+
+                listing.setTitle(rs.getString("title"));
+                listing.setZip(rs.getString("zip"));
+                listing.setType("type");
+                listing.setSex("sex");
+                listing.setAge("age");
+                listing.setDesc("desc");
+
+                String path = rs.getString("photo");
+                File file = new File(path);
+                listing.setPhoto(file);
+
+                return Optional.of(listing);
+            }
+        } catch (SQLException e) {
+            // TODO Handle errors
+        } finally {
+            try {
+                if (ps != null) {
+                    ps.close();
+                }
+            } catch (SQLException e) {
+                // TODO Handles errors
+            }
+        }
+        return Optional.empty();
+    }
+
+    public Optional<Listing> selectListing(int lid) {
+        String LISTING_QUERY = "SELECT * FROM `listing` WHERE `lid` = ?;";
+        PreparedStatement ps = null;
+        try {
+            ps = database.getPreparedStatement(LISTING_QUERY);
             ps.setInt(1, lid);
             ResultSet rs = ps.executeQuery();
             if (rs.next()) {
@@ -141,6 +411,14 @@ public class DatabaseManager {
             }
         } catch (SQLException e) {
             // TODO Handle errors
+        } finally {
+            try {
+                if (ps != null) {
+                    ps.close();
+                }
+            } catch (SQLException e) {
+                // TODO Handles errors
+            }
         }
         return Optional.empty();
     }
@@ -153,8 +431,9 @@ public class DatabaseManager {
                 + "AND `type` LIKE ? "
                 + "AND `sex` LIKE ? "
                 + "AND `age` = ?;";
+        PreparedStatement ps = null;
         try {
-            PreparedStatement ps = database.getPreparedStatement(LISTING_QUERY);
+            ps = database.getPreparedStatement(LISTING_QUERY);
             ps.setString(1, title);
             ps.setString(2, zip);
             ps.setString(3, type);
@@ -181,6 +460,14 @@ public class DatabaseManager {
             }
         } catch (SQLException e) {
             // TODO Handle errors
+        } finally {
+            try {
+                if (ps != null) {
+                    ps.close();
+                }
+            } catch (SQLException e) {
+                // TODO Handles errors
+            }
         }
         return listings;
 	}
--- src/main/java/com/jkmalan/adoptafriend/user/Account.java
+++ src/main/java/com/jkmalan/adoptafriend/user/Account.java
@@ -1,13 +1,19 @@
 package com.jkmalan.adoptafriend.user;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
 public class Account {
 
     private final int uid;
 
     private String hash;
+    private byte[] salt;
 
-    public Account(int uid, String hash) {
+    public Account(int uid, byte[] salt, String hash) {
         this.uid = uid;
+        this.salt = salt;
         this.hash = hash;
     }
 
@@ -21,6 +27,38 @@ public class Account {
 
     public void setHash(String hash) {
         this.hash = hash;
+    }
+
+    private void setSalt(byte[] salt) {
+        this.salt = salt;
+    }
+
+    public String createHash(String message) {
+        String hash = null;
+        try {
+            if (salt == null) {
+                setSalt(createSalt());
+            }
+
+            MessageDigest md = MessageDigest.getInstance("SHA-1");
+            md.update(salt);
+            byte[] bytes = md.digest(message.getBytes());
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < bytes.length; i++) {
+                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            hash = sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            // TODO Handle errors
+        }
+        return hash;
+    }
+
+    private byte[] createSalt() throws NoSuchAlgorithmException {
+        SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");
+        byte[] salt = new byte[16];
+        sr.nextBytes(salt);
+        return salt;
     }
 
 }
